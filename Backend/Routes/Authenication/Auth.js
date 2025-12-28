86d34efc64b866d5b0bab743ae2d3d1316f98386
Dhruv <dhruvnkejriwal@gmail.com>
2025-12-28 15:04:46 +0530

    User SignUp Route

--- Backend/Routes/Authenication/Auth.js
+++ Backend/Routes/Authenication/Auth.js
@@ -1,5 +1,6 @@
 import express from "express"; 
 import dotenv from "dotenv";  
+import CreateUser from "../../Database/Users/CreateUser"
 
 dotenv.config(); 
 const router = express.Router(); 
@@ -9,20 +10,14 @@ router.post('/Auth/Signup', async (req, res) => {
     const { Email , Password , Username  } = req.body;
     if (!Email || !Password || !Username) {
       return res.status(400).json({ message: "All fields are required" });
-    }
-    
-    const existingUser = await GetUser(Email); 
-    if (existingUser) {
-      return res.status(409).json({ message: "User already exists" });
-    }
+      } 
     const newUser = {
         Email,
         Password, 
         Username, 
         Role : "User" , 
-        createdAt: new Date()
       };   
-    const result = await CreateUser([newUser]); 
+    const result = await CreateUser(newUser); 
     return res.status(201).json({
       message: "User added successfully",
       insertedId: result.insertedId
@@ -32,3 +27,5 @@ router.post('/Auth/Signup', async (req, res) => {
     return res.status(500).json({ error: "Internal server error while adding data" });
   }
 }); 
+
+
