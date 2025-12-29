cc8528702376d75a1ef7e895c49c64a4e59231b3
Dhruv <dhruvnkejriwal@gmail.com>
2025-12-29 23:57:05 +0530

    Validating Property

---
+++ Backend/Database/Property/Post/ValidateProperty.js
@@ -0,0 +1,40 @@
+import supabase from "../../SupabaseClient.js";
+
+const ValidateProperty = async (propertyId, data, adminUser) => {
+  if (!data.transactionHash) {
+    throw new Error("Minting not confirmed on blockchain");
+  }
+  if (!data.priceINR || !data.tokenQuantity) {
+    throw new Error("Invalid token economics");
+  }
+  const pricePerTokenINR = Math.floor(
+    data.priceINR / data.tokenQuantity
+  );
+  const { data: property, error } = await supabase
+    .from("properties")
+    .update({
+      launched_price_inr: data.priceINR,
+      price_per_token_inr: pricePerTokenINR,
+
+      token_name: data.tokenName,
+      initial_token_quantity: data.tokenQuantity,
+      token_quantity: data.tokenQuantity,
+
+      is_tokenized: true,
+      transaction_hash: data.transactionHash,
+
+      validated_by: adminUser.id,
+      validated_at: new Date(),
+
+      status: "validated",
+      is_listed: true
+    })
+    .eq("id", propertyId)
+    .select()
+    .single();
+
+  if (error) throw error;
+  return property;
+};
+
+export default ValidateProperty;
